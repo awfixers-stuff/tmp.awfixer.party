63a6bb600b91232f12721cf7a8e425a853f38e61
awfixer <git@awfixer.me>
2026-04-21 12:41:34 -0700

    feat: update tmpsite

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,10 @@
 import createMDX from "@next/mdx"
-import rehypeSlug from "rehype-slug"
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 }
 
-const withMDX = createMDX({
-  options: {
-    rehypePlugins: [rehypeSlug],
-  },
-})
+const withMDX = createMDX()
 
 export default withMDX(nextConfig)
